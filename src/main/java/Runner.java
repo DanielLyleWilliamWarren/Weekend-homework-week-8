03552c6bb7b55affd7886f561bb08c1616b95b07
DanielLyleWilliamWarren <daniel.warren.deen@gmail.com>
2018-07-13 14:14:59 +0100

    setup runner

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,2 +1,18 @@
+import db.DBHelper;
+import models.League;
+import models.Team;
+
 public class Runner {
+
+    public static void main(String[] args) {
+        League league = new League("La Liga");
+        DBHelper.save(league);
+
+        Team team1 = new Team("Valencia", 0, 1000000.00, league);
+        DBHelper.save(team1);
+        Team team2 = new Team("Barcelona", 0, 1000000.00, league);
+        DBHelper.save(team1);
+    }
+
+
 }
