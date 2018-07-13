c8ea55d416200de67a86ad6805fb226e6f4a77fc
DanielLyleWilliamWarren <daniel.warren.deen@gmail.com>
2018-07-13 12:18:35 +0100

    setup Staff class

--- src/main/java/models/Manager.java
+++ src/main/java/models/Manager.java
@@ -1,4 +1,13 @@
 package models;
 
 public class Manager extends Staff {
+
+    private int popularity;
+
+    public Manager(){}
+
+    public Manager(String name, double salary, int popularity){
+        super(name, salary);
+        this.popularity = popularity;
+    }
 }
--- src/main/java/models/Staff.java
+++ src/main/java/models/Staff.java
@@ -1,4 +1,49 @@
 package models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "staff")
 public abstract class Staff {
+
+    private int id;
+    private String name;
+    private double salary;
+
+    public Staff() {
+    }
+
+    public Staff(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "salary")
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 }
