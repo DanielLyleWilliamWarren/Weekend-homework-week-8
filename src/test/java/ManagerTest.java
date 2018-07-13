0dd5e78eb6cf1982cf22a7c25950ec841602304d
DanielLyleWilliamWarren <daniel.warren.deen@gmail.com>
2018-07-13 13:01:40 +0100

    manager passing all tests

--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -1,2 +1,41 @@
+import models.League;
+import models.Manager;
+import models.Team;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ManagerTest {
+
+    Manager manager;
+    Team rangers;
+    League spl;
+
+    @Before
+    public void setup(){
+        spl = new League("Scottish Premiership");
+        rangers = new Team("New Co", 0, -500000, spl);
+        manager = new Manager("Neil Lennon", 50000.00, 50, rangers);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Neil Lennon", manager.getName());
+    }
+
+    @Test
+    public void hasSalary(){
+        assertEquals(50000.00, manager.getSalary(),1);
+    }
+
+    @Test
+    public void hasPopularityRating(){
+        assertEquals(50, manager.getPopularity());
+    }
+
+    @Test
+    public void hasTeam(){
+        assertEquals(rangers, manager.getTeam());
+    }
 }
