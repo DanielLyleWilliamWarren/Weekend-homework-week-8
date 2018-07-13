ce55c9e22c01a4e1dae8e4ced6c653a4723d6233
DanielLyleWilliamWarren <daniel.warren.deen@gmail.com>
2018-07-13 12:32:54 +0100

    setup Manager

--- src/main/java/models/Manager.java
+++ src/main/java/models/Manager.java
@@ -1,13 +1,39 @@
 package models;
 
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
 public class Manager extends Staff {
 
     private int popularity;
+    private Team team;
 
     public Manager(){}
 
-    public Manager(String name, double salary, int popularity){
+    public Manager(String name, double salary, int popularity, Team team){
         super(name, salary);
         this.popularity = popularity;
+        this.team = team;
+    }
+
+    @Column(name = "popularity")
+    public int getPopularity(){
+        return this.popularity;
+    }
+
+    public void setPopularity(int popularity) {
+        this.popularity = popularity;
+    }
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id", nullable = false)
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
     }
 }
